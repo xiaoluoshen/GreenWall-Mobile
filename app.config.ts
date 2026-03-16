9a241c1140492bfdaab4a1dc62972dd90708bef2
xiaoluoshen <105665581+xiaoluoshen@users.noreply.github.com>
2026-03-16 15:18:56 -0400

    chore: update version to 1.2.0 in app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -41,7 +41,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.1.2",
+  version: "1.2.0",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
