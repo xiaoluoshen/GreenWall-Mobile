7aaec010baad66bb29f4263c0fa092d2762c75d6
xiaoluoshen <1.05665581e+08+xiaoluoshen@users.noreply.github.com>
2026-08-13 21:51:50 +0000

    chore(release): v1.2.1

--- app.config.ts
+++ app.config.ts
@@ -36,7 +36,7 @@ const env = {
 const config: ExpoConfig = {
   name: env.appName,
   slug: env.appSlug,
-  version: "1.2.0",
+  version: "1.2.1",
   orientation: "portrait",
   icon: "./assets/images/icon.png",
   scheme: env.scheme,
